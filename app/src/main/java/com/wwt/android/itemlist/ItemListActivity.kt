11c5c61ae00e4fdb4eacad43a2faf681ca91d3e8
Tom Hermann <tom.hermann@wwt.com>
2022-02-21 17:44:59 -0600

    Upgrade SDK level, and bumped dependencies.

--- app/src/main/java/com/wwt/android/itemlist/ItemListActivity.kt
+++ app/src/main/java/com/wwt/android/itemlist/ItemListActivity.kt
@@ -2,13 +2,17 @@ package com.wwt.android.itemlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wwt.android.itemlist.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ItemListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val model: ItemListViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/test/java/com/wwt/android/itemlist/ItemListActivityTest.kt
+++ app/src/test/java/com/wwt/android/itemlist/ItemListActivityTest.kt
@@ -1,15 +1,7 @@
 package com.wwt.android.itemlist
 
-import android.app.Activity
-import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.list_item.view.*
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
@@ -18,6 +10,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
+import org.mockito.kotlin.mock
 import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
