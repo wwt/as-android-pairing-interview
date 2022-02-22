fd1adc7ef09a98fdc3945b2a9834c397054ea8b1
Tom Hermann <tom.hermann@wwt.com>
2022-02-21 20:59:58 -0600

    Wrote single example test, because working with recycler views and espresso matchers isn't the most elegant thing.

--- app/src/test/java/com/wwt/android/itemlist/ItemListActivityTest.kt
+++ app/src/test/java/com/wwt/android/itemlist/ItemListActivityTest.kt
@@ -1,8 +1,13 @@
 package com.wwt.android.itemlist
 
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import io.kotest.matchers.shouldNotBe
+import com.wwt.android.matchers.atPosition
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,13 +15,12 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
-import org.mockito.kotlin.mock
 import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28])
 class ItemListActivityTest : AutoCloseKoinTest() {
-    private val itemListViewModel: ItemListViewModel = mock()
+    private val itemListViewModel: ItemListViewModel = ItemListViewModel()
 
     @Before
     fun setUp() {
@@ -26,9 +30,14 @@ class ItemListActivityTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun `Example test`() {
-        launchActivity<ItemListActivity>().onActivity {
-            it shouldNotBe null
+    fun `should add item and clear input field`() {
+        launchActivity<ItemListActivity>().use {
+            onView(withId(R.id.newItemInput)).perform(typeText("New Item"))
+
+            onView(withId(R.id.addButton)).perform(click())
+
+            onView(withId(R.id.newItemInput)).check(matches(withText("")))
+            onView(withId(R.id.itemsRecycler)).check(matches(atPosition(0, hasDescendant(withText("New Item")))))
         }
     }
 }
